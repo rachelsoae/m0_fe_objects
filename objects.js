93d473fdadfc4d3494ee81bc0d91c78c89096828
Rachel Prather <rachelprather1@gmail.com>
2023-03-02 16:19:35 -0800

    Complete Part 1 of object assignment

--- objects.js
+++ objects.js
@@ -13,19 +13,20 @@ var foods = {
 
 // Write code that prints all of the 'keys' of the foods variable 
 // you created above:
-// YOUR CODE HERE
+console.log(Object.keys(foods));
 
 // Write code that prints all of the 'values' of the foods variable 
 // you created above:
-// YOUR CODE HERE
+console.log(Object.values(foods));
 
 // Write code that prints the value of the second food of the foods variable 
 // you created above:
-// YOUR CODE HERE
+console.log(foods.grapes);
 
 // Write code that adds a food to the foods object. 
 // Then, print the updated object:
-// YOUR CODE HERE
+foods.milk = "delicous"
+console.log(foods.milk);
 
 
 // -------------------
